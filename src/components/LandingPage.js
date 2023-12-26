a1f3ebc7fb00dcf326bf0f7c7c9bb69c7bc27cd1
styp3r <stypertypes@gmail.com>
2023-12-26 14:42:26 +0530

    Creates navbar with non-functional links to pages and hero section with hero image

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,16 @@
+import Navbar from './Navbar'
+import SquareLogo from '../images/navut_logo_square.jpg'
+import HeroImg from '../images/heroImg.jpg'
+
 const LandingPage = () => {
     return (
-        <div id = 'landingPage'>
-            
+        <div id='landingPage'>
+            <img id="squareLogo" width="130" height="130" alt="logo" src={SquareLogo}></img>
+            <Navbar />
+            <div className="heroSection">
+                <img id="heroImg" alt="heroImage" src={HeroImg}></img>
+            </div>
+
         </div>
     )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () =>{
+    return(
+        <div id = "navbar">
+            <div className = "navbarItems">
+                <div className = "navbarItems_container">
+                <p className = "navbarLink">Home</p>
+                <p className = "navbarLink">Bookings</p>
+                <p className = "navbarLink">About Us</p>
+                <p className = "navbarLink">Contact Us</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
