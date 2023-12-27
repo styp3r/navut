828078de96da60643ef6a2cab47dcc482653ae35
styp3r <stypertypes@gmail.com>
2023-12-27 16:23:19 +0530

    Creates Amenities section in landing page with symbols and decoration

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from './Navbar'
 import Hero from './Hero'
 import Gallery from './Gallery'
+import PropertyInfo from './PropertyInfo'
 import SquareLogo from '../images/navut_logo_square.jpg'
 
 const LandingPage = () => {
@@ -11,6 +12,7 @@ const LandingPage = () => {
             <Navbar />
             <Hero />
             <Gallery />
+            <PropertyInfo />
         </div>
     )
 }
---
+++ src/components/PropertyInfo.js
@@ -0,0 +1,42 @@
+import Leaf3 from '../images/decoration/leaf3.png'
+
+const PropertyInfo = () => {
+    return (
+        <div>
+            <p style = {{color: '#996132', fontSize: "2.5rem",fontFamily: "'Caveat', cursive"}}>Amenities</p>
+            <div id="propertyInfo">
+                <div className="amenList">
+                    <div className="amenBlocks">
+                        <div className="amenBlockLeft">
+                            <p className="amenItem"><span class="material-symbols-outlined">local_parking</span>Parking</p>
+                            <p className="amenItem"><span class="material-symbols-outlined">wifi</span>Wifi</p>
+                            <p className="amenItem"><span class="material-symbols-outlined">psychiatry</span>Coffee Plantation</p>
+                            <p className="amenItem"><span class="material-symbols-outlined">landscape</span>Valley View</p>
+                            <p className="amenItem"><span class="material-symbols-outlined">pets</span>Pets Allowed</p>
+                            <p className="amenItem"><span class="material-symbols-outlined">restaurant</span>Restaurant</p>
+                            <p className="amenItem"><span class="material-symbols-outlined">table_lamp</span>Dedicated Workspace</p>
+                        </div>
+                        <div className="amenBlockRight">
+                            <p className="amenItem"><span class="material-symbols-outlined">device_thermostat</span>Hot Water</p>
+                            <p className="amenItem"><span class="material-symbols-outlined">self_care</span>Essentials</p>
+                            <p className="amenItem"><span class="material-symbols-outlined">balcony</span>Balcony</p>
+                            <p className="amenItem"><span class="material-symbols-outlined">deck</span>Terrace</p>
+                            <p className="amenItem"><span class="material-symbols-outlined">concierge</span>Self Check-In</p>
+                            <p className="amenItem"><span class="material-symbols-outlined">smoking_rooms</span>Smoking Allowed</p>
+                            <p className="amenItem"><span class="material-symbols-outlined">accessibility</span>Building Staff</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="amenContent">
+                    <img className="leaf3Img" src={Leaf3}></img>
+                    <p style={{ margin: '3.5rem 10rem 2rem 0', textAlign: 'left' }}>Immerse yourself in nature's embrace as you gaze upon the breathtaking forest and valley views from the comfort of your room. Nestled amidst lush greenery and rolling hills, our location offers a symphony of sights that redefine tranquility and serenity.</p>
+                    <p style={{ margin: '0 10rem 0 0', textAlign: 'left' }}>Explore the city with ease using our concierge services, which provide insider knowledge and assistance in planning your excursions and experiences.</p>
+                    <p style={{ margin: '2rem 10rem 2rem 0', textAlign: 'left' }}>At Navut, our commitment to your comfort and satisfaction is unwavering. Elevate your stay with our array of amenities, meticulously curated to cater to your every need.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PropertyInfo;
