ca5e988bb1783ed114371d1e84cbb6b0bfd823ca
styp3r <stypertypes@gmail.com>
2023-12-27 17:23:22 +0530

    Testimonials section redesign

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,15 +1,54 @@
 const testimonials = [
-    {
-      id: 1,
-      name: 'John Doe',
-      text: 'This is a fantastic slider!',
-    },
-    {
-      id: 2,
-      name: 'Jane Smith',
-      text: 'I love the design and functionality.',
-    },
-    // Add more testimonials as needed
-  ];
+  {
+    id: 1,
+    name: 'Rajesh Kumar',
+    text: 'Staying at the Navut homestay was a serene experience. The lush greenery and the hospitality were exceptional.',
+  },
+  {
+    id: 2,
+    name: 'Priya Singh',
+    text: 'This homestay in Coorg offered a perfect blend of nature and comfort. The sunrise views were breathtaking!',
+  },
+  {
+    id: 3,
+    name: 'Amit Patel',
+    text: 'I was charmed by the tranquility of the homestay. The hosts were warm, and the local cuisine was delightful.',
+  },
+  {
+    id: 4,
+    name: 'Sneha Rao',
+    text: 'The homestay provided an escape from the city hustle. I loved exploring Coorg and returning to such a cozy place.',
+  },
+  {
+    id: 5,
+    name: 'Karthik Menon',
+    text: "A memorable stay in Coorg! The homestay's ambiance made me feel at home amidst nature's beauty.",
+  },
+  {
+    id: 6,
+    name: 'Ayesha Khan',
+    text: "I thoroughly enjoyed my time in Coorg. The homestay's location and hospitality made it an unforgettable trip.",
+  },
+  {
+    id: 7,
+    name: 'Vikram Sharma',
+    text: "Coorg's homestay was a retreat for the soul. The hosts were gracious, and the surroundings were captivating.",
+  },
+  {
+    id: 8,
+    name: 'Neha Gupta',
+    text: "The stay in Coorg exceeded my expectations. The homestay's authenticity and comfort were beyond compare.",
+  },
+  {
+    id: 9,
+    name: 'Anand Verma',
+    text: "Navut's homestay was a hidden gem. The tranquility and the warm hospitality made it a rejuvenating experience.",
+  },
+  {
+    id: 10,
+    name: 'Ritu Desai',
+    text: 'The stay in Coorg was enchanting. Waking up to the sounds of nature at the homestay was truly magical.',
+  }
+];
 
-  export default testimonials;
+export default testimonials;
--- src/components/TestimonialSlider.js
+++ src/components/TestimonialSlider.js
@@ -1,41 +1,46 @@
 import React, { useState } from 'react';
 import Reviews from './Reviews'
+import QuoteSymbol from '../images/decoration/quoteSymbol.png'
 
 const TestimonialSlider = () => {
 
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [hideTestimonial, setHideTestimonial] = useState(false);
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [hideTestimonial, setHideTestimonial] = useState(false);
 
-  const prevSlide = () => {
-    setHideTestimonial(true);
-    setTimeout(() => {
-      const newIndex = (currentSlide - 1 + Reviews.length) % Reviews.length;
-      setCurrentSlide(newIndex);
-      setHideTestimonial(false);
-    }, 500); // Ensure this matches the transition duration in CSS
-  };
+    const prevSlide = () => {
+        setHideTestimonial(true);
+        setTimeout(() => {
+            const newIndex = (currentSlide - 1 + Reviews.length) % Reviews.length;
+            setCurrentSlide(newIndex);
+            setHideTestimonial(false);
+        }, 500); // Ensure this matches the transition duration in CSS
+    };
 
-  const nextSlide = () => {
-    setHideTestimonial(true);
-    setTimeout(() => {
-      const newIndex = (currentSlide + 1) % Reviews.length;
-      setCurrentSlide(newIndex);
-      setHideTestimonial(false);
-    }, 500); // Ensure this matches the transition duration in CSS
-  };
+    const nextSlide = () => {
+        setHideTestimonial(true);
+        setTimeout(() => {
+            const newIndex = (currentSlide + 1) % Reviews.length;
+            setCurrentSlide(newIndex);
+            setHideTestimonial(false);
+        }, 500); // Ensure this matches the transition duration in CSS
+    };
 
-  return (
-    <div className="testimonial-slider">
-      <div className={`testimonial ${hideTestimonial ? 'hidden' : ''}`}>
-        <p className="testimonial-text">{Reviews[currentSlide].text}</p>
-        <p className="testimonial-name">- {Reviews[currentSlide].name}</p>
-      </div>
-      <div className = "slideBtns">
-      <button className = "prevBtn" onClick={prevSlide}><span class="material-symbols-outlined">chevron_left</span></button>
-      <button className = "nextBtn" onClick={nextSlide}><span class="material-symbols-outlined">chevron_right</span></button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="testimonial-slider">
+            <p style = {{color: '#996132', fontSize: "2.5rem",fontFamily: "'Caveat', cursive"}}>Testimonials</p>
+            <img className = "quoteSymbol" src = {QuoteSymbol}></img>
+            <div className = "testimonialCard">
+                <div className={`testimonial ${hideTestimonial ? 'hidden' : ''}`}>
+                    <p className="testimonial-text">{Reviews[currentSlide].text}</p>
+                    <p style = {{fontStyle: 'italic', fontSize: '1.2rem'}} className="testimonial-name">- {Reviews[currentSlide].name}</p>
+                </div>
+            </div>
+            <div className="slideBtns">
+                <button className="prevBtn" onClick={prevSlide}><span class="material-symbols-outlined">chevron_left</span></button>
+                <button className="nextBtn" onClick={nextSlide}><span class="material-symbols-outlined">chevron_right</span></button>
+            </div>
+        </div>
+    );
 };
 
 export default TestimonialSlider;
