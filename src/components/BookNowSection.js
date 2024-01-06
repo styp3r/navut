6b7c8dd253ee263bf94c9fe01fc9afba1fd374c5
styp3r <stypertypes@gmail.com>
2024-01-06 23:36:59 +0530

    Inserts a background image for bookNow section

--- src/components/BookNowSection.js
+++ src/components/BookNowSection.js
@@ -1,6 +1,9 @@
+import Leaves from '../images/decoration/leaves.jpg'
+
 const BookNowSection = () => {
     return (
         <div id="bookNowSection">
+            <img alt = "autumn leaves" src = {Leaves} className = "leavesDecor"></img>
             <p className="bookNowText">Indulge in the perfect blend of comfort and authenticity - book your stay at our homestay for an unforgettable experience immersed in local charm and heartfelt hospitality</p>
             <button id="bookNowBtnLanding">Book Now</button>
         </div>
