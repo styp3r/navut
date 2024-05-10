b1008cdad50fa76b0f7cf79f36ae8bdfc87cf949
styp3r <stypertypes@gmail.com>
2024-05-10 14:31:56 +0530

    Transfers api keys to env

--- src/components/supabase.js
+++ src/components/supabase.js
@@ -1,8 +1,8 @@
 // yourSupabaseConfiguration.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tbfhmugkjiiwrlavunae.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRiZmhtdWdramlpd3JsYXZ1bmFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYwOTcyMTUsImV4cCI6MjAyMTY3MzIxNX0.I6jAJI-1M_VUpbvcVhOxiWA4_gVuTvIdg4KMT8yn3MU';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
