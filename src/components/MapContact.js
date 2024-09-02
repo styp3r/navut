e9d4b1ef3eece690d460a09ec5d110a9a3303f6e
styp3r <stypertypes@gmail.com>
2024-09-02 14:16:22 +0530

    Adds scrolling functionality to address unintended map scroll while scrolling page - map scroll is activated after a tap on the map view.

--- src/components/MapContact.js
+++ src/components/MapContact.js
@@ -1,8 +1,24 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvent } from 'react-leaflet'
 
 const MapContact = () => {
+
+  // Custom hook to manage dragging behavior
+  const EnableDraggingOnClick = () => {
+    const map = useMapEvent('click', () => {
+      map.dragging.enable();
+    });
+
+    useMapEvent('mouseout', () => {
+      map.dragging.disable();
+    });
+
+    return null;
+  };
+
   return (
-    <MapContainer id="mapContact" center={[12.189283475968463, 75.74842653994045]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer id="mapContact" center={[12.189283475968463, 75.74842653994045]} zoom={13} scrollWheelZoom={false} dragging={false}>
+
+      <EnableDraggingOnClick />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -10,7 +26,7 @@ const MapContact = () => {
       <Marker position={[12.189283475968463, 75.74842653994045]}>
         <Popup>
           <p>Navut Homestay</p>
-          <a target = "_blank" rel="noreferrer" href = "https://maps.app.goo.gl/iKeZKYX7rrkuHwKA8">Get Directions</a>
+          <a target="_blank" rel="noreferrer" href="https://maps.app.goo.gl/iKeZKYX7rrkuHwKA8">Get Directions</a>
         </Popup>
       </Marker>
     </MapContainer>
